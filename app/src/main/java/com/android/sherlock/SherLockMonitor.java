fbff089cf86981f92ab82d21c937995a97e40f4c
wusongyuan <wusongyuan@taqu.cn>
2021-12-17 19:48:01 +0800

    feat: hook进程、安装列表、剪切板

--- app/src/main/java/com/android/sherlock/SherLockMonitor.java
+++ app/src/main/java/com/android/sherlock/SherLockMonitor.java
@@ -1,6 +1,12 @@
 package com.android.sherlock;
 
+import android.app.ActivityManager;
+import android.content.ClipboardManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.LocationManager;
+import android.os.UserHandle;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -38,6 +44,56 @@ public class SherLockMonitor  implements IXposedHookLoadPackage {
             return;
         }
 
+        hookDeviceId(lpparam);
+
+        hookMacAddress(lpparam);
+
+        //hook定位方法
+        XposedHelpers.findAndHookMethod(
+                LocationManager.class.getName(),
+                lpparam.classLoader,
+                "getLastKnownLocation",
+                String.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) {
+                        XposedBridge.log("调用getLastKnownLocation获取了GPS地址");
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log(getMethodStack());
+                        super.afterHookedMethod(param);
+                    }
+                }
+        );
+
+        hookProcesses(lpparam);
+
+        hookInstallList(lpparam);
+
+        //hook剪切板
+        XposedHelpers.findAndHookMethod(
+                ClipboardManager.class.getName(),
+                lpparam.classLoader,
+                "getPrimaryClip",
+                Integer.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) {
+                        XposedBridge.log("调用getPrimaryClip");
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log(getMethodStack());
+                        super.afterHookedMethod(param);
+                    }
+                }
+        );
+    }
+
+    private void hookDeviceId(XC_LoadPackage.LoadPackageParam lpparam) {
         //hook获取设备信息方法
         XposedHelpers.findAndHookMethod(
                 android.telephony.TelephonyManager.class.getName(),
@@ -48,6 +104,44 @@ public class SherLockMonitor  implements IXposedHookLoadPackage {
                     @Override
                     protected void beforeHookedMethod(MethodHookParam param) {
                         XposedBridge.log("调用getDeviceId(int)获取了imei");
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log(getMethodStack());
+                        super.afterHookedMethod(param);
+                    }
+                }
+        );
+
+        //hook获取设备信息方法
+        XposedHelpers.findAndHookMethod(
+                android.telephony.TelephonyManager.class.getName(),
+                lpparam.classLoader,
+                "getDeviceId",
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) {
+                        XposedBridge.log("调用getDeviceId获取了imei");
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log(getMethodStack());
+                        super.afterHookedMethod(param);
+                    }
+                }
+        );
+
+        //hook获取设备信息方法
+        XposedHelpers.findAndHookMethod(
+                android.telephony.TelephonyManager.class.getName(),
+                lpparam.classLoader,
+                "getSimSerialNumber",
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) {
+                        XposedBridge.log("调用getSimSerialNumber");
                     }
 
                     @Override
@@ -77,6 +171,10 @@ public class SherLockMonitor  implements IXposedHookLoadPackage {
                     }
                 }
         );
+    }
+
+    private void hookMacAddress(XC_LoadPackage.LoadPackageParam lpparam) {
+
         //hook低版本系统获取mac地方方法
         XposedHelpers.findAndHookMethod(
                 android.net.wifi.WifiInfo.class.getName(),
@@ -113,17 +211,19 @@ public class SherLockMonitor  implements IXposedHookLoadPackage {
                     }
                 }
         );
+    }
 
-        //hook定位方法
+    private void hookProcesses(XC_LoadPackage.LoadPackageParam lpparam) {
+        //hook
         XposedHelpers.findAndHookMethod(
-                LocationManager.class.getName(),
+                ActivityManager.class.getName(),
                 lpparam.classLoader,
-                "getLastKnownLocation",
-                String.class,
+                "getRunningTasks",
+                Integer.class,
                 new XC_MethodHook() {
                     @Override
                     protected void beforeHookedMethod(MethodHookParam param) {
-                        XposedBridge.log("调用getLastKnownLocation获取了GPS地址");
+                        XposedBridge.log("调用getRunningTasks");
                     }
 
                     @Override
@@ -132,6 +232,192 @@ public class SherLockMonitor  implements IXposedHookLoadPackage {
                         super.afterHookedMethod(param);
                     }
                 }
+        );
+
+        //hook
+        XposedHelpers.findAndHookMethod(
+                ActivityManager.class.getName(),
+                lpparam.classLoader,
+                "getRunningAppProcesses",
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) {
+                        XposedBridge.log("调用getRunningAppProcesses");
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log(getMethodStack());
+                        super.afterHookedMethod(param);
+                    }
+                }
+        );
+    }
+
+    /**
+     * 获取安装列表
+     *
+     * @param lpparam
+     */
+    private void hookInstallList(XC_LoadPackage.LoadPackageParam lpparam) {
+        //hook
+        XposedHelpers.findAndHookMethod(
+                PackageManager.class.getName(),
+                lpparam.classLoader,
+                "getInstalledPackages",
+                Integer.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) {
+                        XposedBridge.log("调用getInstalledPackages");
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log(getMethodStack());
+                        super.afterHookedMethod(param);
+                    }
+                }
+        );
+        //hook
+        XposedHelpers.findAndHookMethod(
+                PackageManager.class.getName(),
+                lpparam.classLoader,
+                "queryIntentActivities",
+                Intent.class,
+                Integer.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) {
+                        XposedBridge.log("调用queryIntentActivities");
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log(getMethodStack());
+                        super.afterHookedMethod(param);
+                    }
+                }
+
+
+        );
+
+        //hook
+        XposedHelpers.findAndHookMethod(
+                PackageManager.class.getName(),
+                lpparam.classLoader,
+                "queryIntentActivitiesAsUser",
+                Intent.class,
+                Integer.class,
+                Integer.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) {
+                        XposedBridge.log("调用queryIntentActivitiesAsUser");
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log(getMethodStack());
+                        super.afterHookedMethod(param);
+                    }
+                }
+
+
+        );
+
+        //hook
+        XposedHelpers.findAndHookMethod(
+                PackageManager.class.getName(),
+                lpparam.classLoader,
+                "queryIntentActivitiesAsUser",
+                Intent.class,
+                Integer.class,
+                UserHandle.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) {
+                        XposedBridge.log("调用queryIntentActivitiesAsUser(,,UserHandle)");
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log(getMethodStack());
+                        super.afterHookedMethod(param);
+                    }
+                }
+
+
+        );
+
+        //hook
+        XposedHelpers.findAndHookMethod(
+                PackageManager.class.getName(),
+                lpparam.classLoader,
+                "queryIntentActivityOptions",
+                ComponentName.class,
+                Integer[].class,
+                ComponentName.class,
+                Integer.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) {
+                        XposedBridge.log("调用queryIntentActivityOptions");
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log(getMethodStack());
+                        super.afterHookedMethod(param);
+                    }
+                }
+
+
+        );
+
+        //hook
+        XposedHelpers.findAndHookMethod(
+                PackageManager.class.getName(),
+                lpparam.classLoader,
+                "getLeanbackLaunchIntentForPackage",
+                String.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) {
+                        XposedBridge.log("调用getLeanbackLaunchIntentForPackage");
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log(getMethodStack());
+                        super.afterHookedMethod(param);
+                    }
+                }
+
+
+        );
+
+        //hook
+        XposedHelpers.findAndHookMethod(
+                PackageManager.class.getName(),
+                lpparam.classLoader,
+                "queryIntentActivities",
+                Intent.class,
+                Integer.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) {
+                        XposedBridge.log("调用queryIntentActivities");
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log(getMethodStack());
+                        super.afterHookedMethod(param);
+                    }
+                }
+
+
         );
     }
 
